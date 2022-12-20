759b6ca8f715708598c41ee52d02b06cc90dec49
armenakyan <arm.vars17@gmail.com>
2022-12-20 21:36:52 +0400

    my hoomework not finished

--- src/components/Varsik/App.jsx
+++ src/components/Varsik/App.jsx
@@ -1,30 +1,15 @@
-import React, {useState, useEffect} from 'react'
-import Product from './Product';
+import React from 'react'
+import Text from './Text';
 
 import "./App.scss"
 
 export default function App() {
-    const [data, setData] = useState([]);
-
-    const handleDelete = (id) => {
-		const newData = data.filter(product => product.id !== id);
-		setData(newData)
-	}
-
-    useEffect(() => {
-        const getData = () => {
-            fetch("https://fakestoreapi.com/products").then(res => res.json()).then(json => setData(json))
-        }
-        getData()
-    }, [])
-
 
   return (
     <div className='List'>
-        {data.map(product => {
-            return <Product key={product.id} product={product} handleDelete={handleDelete}/>
-        })
-        }
+        <Text as={"p"} clName={"box"} theme={"dark"}>
+          My Tag
+        </Text>
     </div>
   )
 }
