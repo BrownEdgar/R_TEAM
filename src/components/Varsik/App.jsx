89e34fe23664a95e7fa9d675ffdda9a4e10d3072
BrownEdgar <nikogosjanedgar@gmail.com>
2022-12-24 08:56:28 +0400

    add reducer example

--- src/components/Varsik/App.jsx
+++ src/components/Varsik/App.jsx
@@ -4,25 +4,6 @@ import Text from './Text';
 import "./App.scss"
 
 export default function App() {
-<<<<<<< HEAD
-    const [data, setData] = useState([]);
-
-    const handleDelete = (id) => {
-		const newData = data.filter(product => product.id !== id);
-		setData(newData)
-	}
-
-    useEffect(() => {
-        const getData = () => {
-            fetch("https://fakestoreapi.com/products")
-						.then(res => res.json())
-						.then(json => setData(json))
-        }
-        getData()
-    }, [])
-
-=======
->>>>>>> 759b6ca8f715708598c41ee52d02b06cc90dec49
 
   return (
     <div className='List'>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/Varsik/App';
-
-
-
+import App from './components/Edgar/HOOKS/Task/Post';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
