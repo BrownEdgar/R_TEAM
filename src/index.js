946de0666facd16dbab625e581d983b7acd8b0b8
Gevorg <gev.grigoryan.@mail.ru>
2023-01-12 22:24:51 +0400

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/Gevorg/Form/App';
+import App from './components/Edgar/FORM/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
