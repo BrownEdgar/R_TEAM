98d9457783acb98332dfa9bf26355dd17fbec65f
BrownEdgar <nikogosjanedgar@gmail.com>
2023-01-12 10:15:04 +0400

    added formik example

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
-import React, { useState } from 'react';
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as yup from 'yup'
+import "./App.css"
 
-import './App.css';
+const validationSchema = yup.object({
+	username: yup
+	.string()
+	.min(3, "too short, needed min 3 symbols")
+	.max(12, "too much symbols, less then 12")
+	.required("required!"),
+	password: yup
+	.string()
+	.required("required!")
+	.matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:"),
+	message: yup.string().required()
+})
 
-function App() {
-	const [value, setValue] = useState(1)
-
-	const actions = (action) => {
-		switch (action.type) {
-			case "minus":
-				setValue(value > 0 ? value - (action?.count ?? 1) : value)
-				break;
-			case "plus":
-				setValue(value < 7 ? value + action.count : value)
-				break;
-			default: return null;
-		}
-	}
-
-	return (
-		<div className="App">
-			<h1>Count: {value}</h1>
-			<button onClick={() => actions({ type: "minus",  count: 2 })} disabled={value === 0}>minus</button>
-			<button onClick={() => actions({ type: "plus", count: 3 })} disabled={value === 7}>add</button>
-		</div>
-	);
+const initialValues = {
+	username: "",
+	password: "",
+	message: "",
 }
 
-export default App;
+export default function App() {
+	const onSubmit = values => {
+		console.log("form data:", values)
+	}
+	
+	return (
+		<Formik 
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={onSubmit}
+			validateOnChange={false}
+			validateOnBlur={true}
+			>
+			<Form>
+				<div>
+					<label htmlFor="username">UserName</label>
+					<Field type="text" id="username" name='username'/>
+					<ErrorMessage name='username' component={"p"}/>
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<Field type="text" id="password" name='password' />
+					<ErrorMessage name="password">
+						{errMsg => <div className="errors">{errMsg}</div>}
+					</ErrorMessage>
+				</div>
+
+				<div>
+					<Field as="textarea" name="message" />
+				</div>
+				<div>
+					<input
+						type="submit"
+						id="submit"
+						name='submit'
+						value="Register"
+					/>
+				</div>
+			</Form>
+		</Formik>
+	)
+}
