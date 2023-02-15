306c005e3f55e04f6807962acff215f85d101cf4
Gevorg <gev.grigoryan.@mail.ru>
2023-02-15 22:19:35 +0400

    homework Lazy example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './components/Arpi/React_Rout_homework/App/App';
+import App from './components/Gevorg/Lazy/AppTest';
 
 
 const container = document.getElementById('root');
