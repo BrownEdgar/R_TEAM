9800e8f322ee22b0cfe308e7c9fcf643b55be79e
BrownEdgar <nikogosjanedgar@gmail.com>
2023-02-25 09:16:42 +0400

    fix index

--- src/App.js
+++ src/App.js
@@ -3,25 +3,9 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-	const [value, setValue] = useState(1)
-
-	const actions = (action) => {
-		switch (action.type) {
-			case "minus":
-				setValue(value > 0 ? value - (action?.count ?? 1) : value)
-				break;
-			case "plus":
-				setValue(value < 7 ? value + action.count : value)
-				break;
-			default: return null;
-		}
-	}
-
 	return (
 		<div className="App">
-			<h1>Count: {value}</h1>
-			<button onClick={() => actions({ type: "minus",  count: 2 })} disabled={value === 0}>minus</button>
-			<button onClick={() => actions({ type: "plus", count: 3 })} disabled={value === 7}>add</button>
+				<h1>netflix</h1>
 		</div>
 	);
 }
--- src/components/Lazy/link.md
+++
@@ -1,7 +0,0 @@
- [video link ](https://youtu.be/JU6sl_yyZqs)
-
-
-
-const arr = [true, 41, 52, [41,12,5, [1,2,3]], {}, 540, [50,60,70]]
-
-
