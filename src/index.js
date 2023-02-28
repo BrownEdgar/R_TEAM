be6508b5d0ed4afbfee763926e1a7e733bb12800
Gevorg <gev.grigoryan.@mail.ru>
2023-02-28 20:53:10 +0400

    website example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
-import App from './components/Gevorg-React/Netflix/Footer/Footer';
+import App from './App';
 
 
 const container = document.getElementById('root');
